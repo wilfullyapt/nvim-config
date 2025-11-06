c470615e9d27b95b87618169c99efe6d3751aa2a
William Mawhinney <wilmawhinney@gmail.com>
2025-11-05 19:45:40 -0800

    small fixes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,11 +1,11 @@
 -- Telescope Remaps
 
 
--- TODO: Depricate?
---local builtin = require('telescope.builtin')
---vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
---vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
---vim.keymap.set('n', '<leader>ps', fucntion builtin.grep_string({search=vim.fn.input("Grep > ")}) end)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = '[T]elescope find fi[l]es' })
+vim.keymap.set('n', '<leader>tt', builtin.git_files, { desc = '[T]elescope gi[t] files' })
+vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = '[T]elescope live [g]rep' })
+--vim.keymap.set('n', '<leader>ts', builtin.grep_string({search=vim.fn.input("Grep > ")}), { desc = '[T]elescope grep [s]tring' } )
 
 
 -- Harpoon Key Maps
